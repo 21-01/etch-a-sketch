e7d4a87aafd108d03b27add4836763375af58bc7
21-01 <banyaklontelontemaudijual+git@gmail.com>
2023-11-09 16:38:24 +0700

    change canvas to 1:1 ratio

--- sketch.js
+++ sketch.js
@@ -1,16 +1,14 @@
 const canvas = document.createElement("div")
 canvas.id = "canvas"
-const canvasSize = document.querySelectorAll(".canvasSize")
-var canvasHeight = document.querySelector("#canvas-height").value
-var canvasWidth = document.querySelector("#canvas-width").value
+var canvasSizes = document.querySelector("#canvasSize").value
 
-function generateCanvas(h, w){
-    var pixelHeightSize = 512/h
-    var pixelWidthSize = 512/w
-    canvas.style.height = "" + (h*pixelHeightSize) + "px"
-    canvas.style.width = "" + (w*pixelWidthSize) + "px"
+function generateCanvas(canvasSizes){
+    var pixelHeightSize = 512/canvasSizes
+    var pixelWidthSize = 512/canvasSizes
+    canvas.style.height = "" + (canvasSizes*pixelHeightSize) + "px"
+    canvas.style.width = "" + (canvasSizes*pixelWidthSize) + "px"
     document.body.appendChild(canvas)
-    var totalPixels = h * w
+    var totalPixels = canvasSizes * canvasSizes
     for (let index = 0; index < totalPixels; index++) {
         const blankPx = document.createElement("div")
         blankPx.className = "pixels"
@@ -26,7 +24,7 @@ function generateCanvas(h, w){
     pixels.forEach((item) => {
         item.onmouseenter = () => {
             console.log(item.style.backgroundColor)
-            item.style.backgroundColor = "black"
+            item.style.backgroundColor = changeColor
         }
     })
 }
@@ -38,12 +36,11 @@ function clearCanvas(){
     })
 }
 
-generateCanvas(canvasHeight, canvasWidth)
+generateCanvas(canvasSizes)
 
 const newCanvasBtn = document.querySelector("#newCanvasBtn")
 newCanvasBtn.addEventListener("click", () => {
     clearCanvas()
-    var canvasNewHeight = document.querySelector("#canvas-height").value
-    var canvasNewWidth = document.querySelector("#canvas-width").value
-    generateCanvas(canvasNewHeight, canvasNewWidth)
+    var canvasNewSize = document.querySelector("#canvasSize").value
+    generateCanvas(canvasNewSize)
 })
